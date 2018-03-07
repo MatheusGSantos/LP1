7983930cc71ab325ab3f83f4775ccdfa0f86054a
Caio Victor Sarmento <cvsarmento@hotmail.com>
2018-03-07 15:10:15 -0300

    Add files via upload

---
+++ 3..c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i,j,tam;
+    char string[50], str[50] = {0};
+
+    printf("Digite a sua palavra: ");
+    gets(string);
+
+    tam = strlen(string);
+    j = tam-1;
+
+
+    for(i = 0; i < tam; i++)
+    {
+        str[i] = string[j];
+        j--;
+    }
+
+    if(strcmp(str, string) == 0)
+        printf("Eh palindromo\n");
+    else
+        printf("Nao eh palindromo\n");
+
+    return 0;
+}
