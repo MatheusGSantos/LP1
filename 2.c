357b0db3ea02aa1462218428240ab7f7d0801e45
Caio Victor Sarmento <cvsarmento@hotmail.com>
2018-03-12 15:48:15 -0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,107 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct{
-  char nome[26];
-  char genero[20];
-  int nIntegrantes;
-  int posRank;
-}tBanda;
+int Fibonacci(int n){
+    if (n == 1 || n == 2){
+        return 1;
+    }
 
-
-void preencheBandas(tBanda *bandas){
-  int i;
-
-  for(i=0; i < 5; i++){
-    printf("Nome da Banda: ");
-    fgets(bandas->nome,26,stdin);
-    bandas->nome[strlen(bandas->nome) - 1] = '\0';
-
-    printf("Genero: ");
-    fgets(bandas->genero,20,stdin);
-    bandas->genero[strlen(bandas->genero) - 1] = '\0';
-
-    printf("Numero de Integrantes: ");
-    scanf("%d", &bandas->nIntegrantes);
-    getchar();
-
-    printf("Posicao no Ranking: ");
-    scanf("%d", &bandas->posRank);
-    getchar();
-    
-    printf("\n");
-    bandas++;
-  }
-  
+    return Fibonacci(n-1) + Fibonacci(n-2);
 }
 
-void exibeDados(tBanda *banda){
-	int rank;
-	
-	printf("Informe o ranking da banda: ");
-	scanf("%d", &rank);
-	getchar();
-	
-	while(1){
-		if(banda->posRank == rank){
-			printf("Nome: %s\n",banda->nome);
-			printf("Genero: %s\n",banda->genero);
-			printf("Numero de Integrantes: %d\n",banda->nIntegrantes);
-			printf("Posicao : %d\n",banda->posRank);		
-			break;
-		}		
-		banda++;
-	}
+int main()
+{
+    int numero, i;
 
-}
+    printf("Digite o tamanho da sequencia: ");
+    scanf("%d", &numero);
 
-void exibeGenero(tBanda *banda){
-	char g[20],i;
-	
-	printf("Informe o genero: ");
-	fgets(g,20,stdin);
-	g[strlen(g) - 1] = '\0';
-	
-	for(i=0; i < 5; i++){
-	  if(strcmp(banda->genero,g) == 0){
-	      printf("%s\n", banda->nome);
-	  }
-	  banda++;  
-	}
-}
-
-int procuraBanda(tBanda *banda){
-	char n[26],i;
-	
-	printf("Informe o nome da banda a ser buscada: ");
-	fgets(n,26,stdin);
-	n[strlen(n) - 1] = '\0';
-	
-	for(i=0; i < 5; i++){
-	  if(strcmp(n,banda->nome) == 0){
-	      return 1;
-	  }
-	      
-	  banda++;  
-	}
-	return 0;
-}
-
-int main(){
-  tBanda Bandas[5];
-  int i;
-  
-  preencheBandas(Bandas);
-  printf("\n");
-  exibeDados(Bandas);
-  printf("\n");
-  exibeGenero(Bandas);
-  printf("\n");
-  if(procuraBanda(Bandas)){
-      printf("Eh banda favorita\n");
-  }else
-      printf("Nao eh banda favorita\n");
-  
-  return 0;
-}
+    for (i = 1; i <= numero; i++){
+        printf("%d\n", Fibonacci(i));
+    }
+    return 0;
+} 
