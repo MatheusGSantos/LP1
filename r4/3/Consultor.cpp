43073642a6806361965ac0d06bafd24a573621c4
HonnyLeonardo <36930357+HonnyLeonardo@users.noreply.github.com>
2018-05-09 23:26:36 -0300

    Add files via upload

---
+++ r4/3/Consultor.cpp
@@ -0,0 +1,35 @@
+
+
+#include "Consultor.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+Consultor::Consultor():Funcionario(){
+}
+
+Consultor::Consultor(string n, string m, double s):Funcionario(n,m,s){
+}
+
+Consultor::~Consultor(){}
+
+double Consultor::getSalario(){
+	return salario*1.1;
+}
+
+double Consultor::getSalario(double percentual){
+	return (salario + salario*percentual);
+}
+
+
+int main(){
+	Funcionario f1("Thais", "12345", 3000.0);
+	Consultor c1("Lincoln", "12356", 3000.0);
+	Funcionario f2("Raoni", "12345", 3000.0);
+
+	cout<< "Salario Thais: " << f1.getSalario() << endl;
+	cout<< "Salario Raoni(Func): " << f2.getSalario() << endl;
+	cout<< "Salario Lincoln(Default): " << c1.getSalario() << endl;
+	cout<< "Salario Lincoln(20%%): " << c1.getSalario(0.2) << endl;
+}
+
