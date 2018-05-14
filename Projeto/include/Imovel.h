3f91156bc288cba0c9a68b3a828d117e7427a6af
jack <36930468+jackineo@users.noreply.github.com>
2018-05-14 15:29:55 -0300

    Add files via upload

---
+++ Projeto/include/Imovel.h
@@ -0,0 +1,21 @@
+#ifndef IMOVEL_H
+#define IMOVEL_H
+
+#include "Endereco.h"
+
+class Imovel
+{
+    private:
+        double valor;
+        Endereco endereco; //Fazer Classe Endereco
+        int tipoOferta;
+
+    public:
+        Imovel();
+        virtual ~Imovel();
+
+        double getValor();
+        int getTipoOferta();
+        Endereco getEndereco();
+        std::string getDescricao();
+}
